b0891fa35432fc6c3e5d63107df8e4a23852a9f7
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-09 22:40:33 +0545

    Connection string file and data access layer has been added.

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DAL
+{
+    public static class DBConnection
+    {
+        public static SqlConnection Connection()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString);
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+            return con;
+        }
+    }
+}
